e571befe175ebf0a9416655f56f79fbe956f40cd
Kusum  Negi <kusumnegi2006@gmail.com>
2021-01-05 18:43:31 +0100

    Changed object names in the main

--- src/basicC++ExerciseQuestions.cpp
+++ src/basicC++ExerciseQuestions.cpp
@@ -22,71 +22,71 @@ int main()
 {
     /*Question 1*/
     std::cout << "Hello World!\n";
-    welcomeText obj;
-    obj.printMessage();
+    welcomeText obj1;
+    obj1.printMessage();
 
     /*Question 2*/
-    sumOfNumbers obj1;
-    obj1.sumNumbers();
+    sumOfNumbers obj2;
+    obj2.sumNumbers();
 
     /*Question 3*/
-    sizeOfOperatorExample obj2;
-    obj2.findSizeOfPrimitiveDataTypes();
+    sizeOfOperatorExample obj3;
+    obj3.findSizeOfPrimitiveDataTypes();
 
     /*Question 4*/
-    checkLimits obj3;
-    obj3.checkLimitsOfPrimitiveDataTypes();
+    checkLimits obj4;
+    obj4.checkLimitsOfPrimitiveDataTypes();
 
     /*Question 5*/
-    calculateVolume obj4;
-    obj4.volume(20); //This will invoke volume of cube
-    obj4.volume(10, 20); //This will invoke volume of Cylinder
-    obj4.volume(10, 20, 30); //This will invoke volume of Cuboid 
+    calculateVolume obj5;
+    obj5.volume(20);         //This will invoke volume of cube
+    obj5.volume(10, 20);     //This will invoke volume of Cylinder
+    obj5.volume(10, 20, 30); //This will invoke volume of Cuboid 
 
     /*Question 6*/
-    modiferTypes obj5;
-    obj5.knowModifier();
+    modiferTypes obj6;
+    obj6.knowModifier();
 
     /*Question 7*/
-    stringExample1 obj6;
-    obj6.printMessage();
-    obj6.stringFuntions();
+    stringExample1 obj7;
+    obj7.printMessage();
+    obj7.stringFuntions();
 
     /*Question 8*/
-    quotientAndRemainder obj7;
-    obj7.findQuotientAndRemainder();
+    quotientAndRemainder obj8;
+    obj8.findQuotientAndRemainder();
 
     /*Question 9*/
-    stringExample2 obj8;
-    obj8.readAndDisplayString();
+    stringExample2 obj9;
+    obj9.readAndDisplayString();
 
     /*Question 10*/
-    rangeBasedForLoop obj9;
-    obj9.printNumbers();
+    rangeBasedForLoop obj10;
+    obj10.printNumbers();
 
     /*Question 11*/
-    switchWithEnumExample obj10;
-    obj10.userFoodChoice();
+    switchWithEnumExample obj11;
+    obj11.userFoodChoice();
 
     /*Question 12*/
-    switchStatementeExample2 obj11;
-    obj11.chooseYourZodiacMessage();
-    obj11.changeZodiacSign();
+    switchStatementeExample2 obj12;
+    obj12.chooseYourZodiacMessage();
+    obj12.changeZodiacSign();
 
     /*Question 13*/
-    mapExample obj12;
-    obj12.addEmployee();
-
-    /*Question 14*/
-    mapExample2 obj13;
+    mapExample obj13;
     obj13.addEmployee();
 
+    /*Question 14*/
+    mapExample2 obj14;
+    obj14.addEmployee();
+
      /*Question 15*/
-    switchStatmentExample3 obj14;
-    obj14.Test();
+    switchStatmentExample3 obj15;
+    obj15.Test();
 
     /*Question 16*/
-    switchStatmentExample4 obj15;
-    obj15.test();
+    switchStatmentExample4 obj16;
+    obj16.test();
 
 }
